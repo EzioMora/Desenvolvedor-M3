28f23d1a949e72313d7db9fa1efebc13cb80778b
Ezio Mora <eziomorabusiness@gmail.com>
2021-05-16 03:17:38 -0300

    feat: webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require("path");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+let config = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public"),
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./src/index.html",
+      minify: true,
+    }),
+    new MiniCssExtractPlugin({
+      filename: "assets/stylesheet/style.css",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "assets/images/[name].[ext]",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [new TerserWebpackPlugin(), new CssMinimizerPlugin()],
+  },
+  devtool: "source-map",
+};
+
+module.exports = config;
