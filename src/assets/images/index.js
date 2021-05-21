f0fa83f85dbd9bb788c3abf6ec82b570b08f2e5f
Ezio Mora <eziomorabusiness@gmail.com>
2021-05-21 08:41:53 -0300

    feat: order styles added

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,3 +1,4 @@
 import "./favicon-m3-16.png";
 import "./logo-m3.png";
 import "./bag.svg";
+import "./icon-x.png";
--- src/assets/scripts/components/order.js
+++ src/assets/scripts/components/order.js
@@ -1,10 +1,14 @@
 // import { updateStore } from "../main.js";
 import { updateCategoryPage } from "../index.js";
 const selectOrderDOM = document.querySelector(".dropdown-order__select");
+const selectOrderListDOM = document.querySelector(".dropdown-order__list");
 const selectOrderTitleDom = document.querySelector(
   ".dropdown-order__selected-title"
 );
 const selectItemsDOM = document.querySelectorAll(".dropdown-order__list-item");
+const btnOrder = document.querySelector(".order__btn");
+const selectOrderMobileDOM = document.querySelector(".dropdown-order");
+const closeOrderMobile = document.querySelector(".dropdown-order__icon-x");
 
 const stateOrder = {
   endpointOrder: "",
@@ -20,17 +24,55 @@ const setStateOrder = ({ resetOrder, setTitleDOM, setEndpoint }) => {
   updateCategoryPage();
 };
 
+let selectDesktopActive = true;
+
+const showSelectDesktop = () => {
+  selectDesktopActive
+    ? selectOrderListDOM.classList.add("show-select-desktop")
+    : selectOrderListDOM.classList.remove("show-select-desktop");
+
+  selectDesktopActive
+    ? selectItemsDOM.forEach((link) =>
+        link.classList.add("show-select-desktop")
+      )
+    : selectItemsDOM.forEach((link) =>
+        link.classList.remove("show-select-desktop")
+      );
+
+  selectDesktopActive = !selectDesktopActive;
+};
+
+selectOrderDOM.addEventListener("click", showSelectDesktop);
+
+let selectMobileActive = true;
+const showSelectMobile = (event) => {
+  selectMobileActive
+    ? selectOrderMobileDOM.classList.add("select-order-mobile")
+    : selectOrderMobileDOM.classList.remove("select-order-mobile");
+
+  selectOrderDOM.removeEventListener("click", showSelectDesktop);
+
+  selectMobileActive = !selectMobileActive;
+};
+
+btnOrder.addEventListener("click", showSelectMobile);
+closeOrderMobile.addEventListener("click", showSelectMobile);
+
 selectItemsDOM.forEach((link) => {
   link.addEventListener("click", (event) => {
     event.preventDefault();
-    const { setSelectOrderTitleDom, setEndpointOrder } = stateOrder;
+    const orderIsMobile = window.innerWidth <= 640;
+    const orderIsDesktop = !orderIsMobile;
     const selectedLink = event.target;
     const linkTitle = selectedLink.innerText;
     const endpoint = selectedLink.dataset.href;
+
     setStateOrder({
       setTitleDOM: linkTitle,
       setEndpoint: endpoint,
     });
+    orderIsDesktop && showSelectDesktop();
+    orderIsMobile && showSelectMobile();
   });
 });
 
