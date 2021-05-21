58b3c55bf2b5e7ed57f0b7f6ea26f3c1f1709648
Ezio Mora <eziomorabusiness@gmail.com>
2021-05-21 09:34:05 -0300

    feat: added pagination component

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,11 +1,21 @@
 import { get } from "./services/HTTPRequests.js";
 import { stateOrder } from "./components/order.js";
 import { createCards, cleanVitrine } from "./components/card.js";
+import {
+  statePagination,
+  setStatePagination,
+} from "./components/pagination.js";
 
 const url = "http://localhost:3004/blusas";
 
 const initCategoryPage = async (endpoint) => {
-  const products = await get(endpoint);
+  let products = await get(endpoint);
+  setStatePagination({
+    totalProducts: products.length,
+  });
+  const { endpointPagination } = statePagination;
+  const newEndpoint = `${url}?${endpointPagination}`;
+  products = await get(newEndpoint);
   createCards(products);
 };
 
@@ -14,7 +24,14 @@ initCategoryPage(url);
 const updateCategoryPage = async () => {
   const { endpointOrder } = stateOrder;
   const endpoint = `${url}?${endpointOrder}`;
-  const products = await get(endpoint);
+  let products = await get(endpoint);
+  setStatePagination({
+    setCurrentPage: 1,
+    totalProducts: products.length,
+  });
+  const { endpointPagination } = statePagination;
+  const newEndpoint = `${endpoint}&${endpointPagination}`;
+  products = await get(newEndpoint);
   cleanVitrine();
   createCards(products);
 };
